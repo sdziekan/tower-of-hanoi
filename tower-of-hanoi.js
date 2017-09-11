3af7eee7fc7f0490b8e28e7b1aac7e6baca3a87a
Salvatore Dziekan <sdziekan@gmail.com>
2017-09-11 15:31:39 +0200

    fixed a few bugs

--- tower-of-hanoi.js
+++ tower-of-hanoi.js
@@ -4,26 +4,25 @@ var array = [[3, 2, 1], [], []];
 
 function move(start, end) {
 
-    return "Invalid move, try again"
+    var win = [[], [], [3, 2, 1]];
+    var a = array[start][(array[start].length - 1)];
+    var b = array[end][(array[end].length - 1)];
 
-    if (start > 2 || end > 2) {
-
-        var win = [[], [], [3, 2, 1]];
-        var a = array[start][(array[start].length - 1)];
-        var b = array[end][(array[end].length - 1)];
-
-        if (array[end].length === 0 || b > a) {
-            array[end].push(a);
-            array[start].pop();
-        } else {
-            console.log("That\'s a terrible move and you are a terrible person. Try again")
-        }
-
-        if (JSON.stringify(array) === JSON.stringify(win)) {
-            console.log("Congratulations\! You are very smart and sexy\!")
-        }
-
-        console.log("array", array)
-        console.log("win", win)
+    if (array[start].length === 0) {
+        console.log("Not a valid move, because you are trying to move a piece that does not exist.")
+    } else if (array[end].length === 0 || b > a) {
+        array[start].pop();
+        array[end].push(a);
+    } else if (end === start) {
+        console.log("Why are you wasting my time? You picked up a piece and put it down in the same spot.")
+    } else {
+        console.log("That\'s a terrible move and you are a terrible person. Try again")
     }
+
+    if (JSON.stringify(array) === JSON.stringify(win)) {
+        console.log("Congratulations\! You are very smart and sexy\!")
+    }
+
+    console.log("array", array)
+    console.log("win", win)
 }
